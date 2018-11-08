254efdfa88af61444cf4dba2bf62bf60fcc6419e
Sqrcz <naorniakowski@slashlab.pl>
2018-11-09 00:24:50 +0100

    bootsrap component example (alert)

--- src/views/PageNotFound.vue
+++ src/views/PageNotFound.vue
@@ -1,5 +1,10 @@
 <template>
     <div class="error-404">
-        Error: 404
+        <b-alert
+            variant="danger"
+            show
+        >
+            Error 404
+        </b-alert>
     </div>
 </template>
