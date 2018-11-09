516bd89f2aa420b55865d403c376a917101f41b3
Tom Naorniakowski <tomasz.naorniakowski@itech.media>
2018-11-09 13:43:45 +0100

    top-bar updated

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,18 +1,28 @@
 <template>
-    <div>
-        <b-nav>
-            <b-nav-item
-                to="/"
-                active
-            >
-                Home
-            </b-nav-item>
-            <b-nav-item to="/dashboard">Dashboard</b-nav-item>
-            <b-nav-item to="/site">Site</b-nav-item>
-            <b-nav-item to="/page">Page</b-nav-item>
-            <b-nav-item to="/trap">It's a Trap!</b-nav-item>
-        </b-nav>
-    </div>
+    <b-navbar
+        fixed="top"
+        tag="header"
+        toggleable="md"
+        type="dark"
+        variant="info"
+    >
+        <b-navbar-brand to="/">MALT</b-navbar-brand>
+        <b-navbar-toggle target="nav_collapse" />
+        <b-collapse
+            id="nav_collapse"
+            is-nav
+        >
+            <b-navbar-nav>
+                <b-nav-item to="/dashboard">Dashboard</b-nav-item>
+                <b-nav-item to="/site">Site</b-nav-item>
+                <b-nav-item to="/page">Page</b-nav-item>
+                <b-nav-item to="/trap">It's a Trap!</b-nav-item>
+            </b-navbar-nav>
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item to="/profile">User</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
 </template>
 
 <script>
@@ -20,7 +30,3 @@ export default {
     name: 'TopBar',
 };
 </script>
-
-<style lang="scss" scoped>
-
-</style>
