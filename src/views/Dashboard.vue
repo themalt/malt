754bea1c1b442a759e88f6ca353a2edb245047c5
lipaaa <michal.lipa@hotmail.com>
2019-01-29 12:21:36 +0100

    create ValidationForm component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,27 +1,20 @@
 <template>
   <div class="dashboard">
     <div class="dashboard__header">Validate your website:</div>
-
-    <div class="validation-section">
-      <b-input-group prepend="Https://" class="validate-form">
-        <b-form-input/>
-        <b-input-group-append>
-          <b-btn variant="info">Validate!</b-btn>
-        </b-input-group-append>
-      </b-input-group>
-    </div>
-
+    <ValidationForm/>
     <ProjectList/>
   </div>
 </template>
 
 <script>
 import ProjectList from "../components/ProjectList.vue";
+import ValidationForm from "../components/ValidationForm.vue";
 
 export default {
   name: "Dashboard",
   components: {
-    ProjectList
+    ProjectList,
+    ValidationForm
   }
 };
 </script>
@@ -38,20 +31,6 @@ export default {
     display: flex;
     justify-content: center;
     font-size: 36px;
-  }
-}
-
-.validation-section {
-  margin-top: 20px;
-  margin-bottom: 20px;
-  width: 70%;
-
-  .input-group-prepend {
-    display: none;
-
-    @media only screen and (min-width: 768px) {
-      display: block;
-    }
   }
 }
 </style>
